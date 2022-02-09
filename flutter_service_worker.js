0d00e819490f8dd3f6f4d9d273b2aae409ee2b7d
samir-benabadji <78525925+samir-benabadji@users.noreply.github.com>
2022-02-08 16:12:03 -0800

    Update

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,7 +3,7 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "assets/AssetManifest.json": "6b8f8de8e51d4b7b9726c0744498785c",
+  "assets/AssetManifest.json": "b693ede699b52eb8b5634eea8879ea36",
 "assets/assets/fonts/agustina.otf": "7b9833076716a8d14eec0cf885a3153c",
 "assets/assets/fonts/GoogleSans-Bold.ttf": "4457817ac2b9993c65e81aa05828fe9c",
 "assets/assets/fonts/GoogleSans-BoldItalic.ttf": "90773b6158663ab0fe78b32680733677",
@@ -53,6 +53,9 @@ const RESOURCES = {
 "assets/assets/images/me.png": "28f46f810ef60993917c4ea1395e6e29",
 "assets/assets/images/microsoft_logo.png": "048685487cd443bd10437eed467c36b0",
 "assets/assets/images/mozilla_logo.png": "f1551e15dce5a9ff23261c07bde2233d",
+"assets/assets/images/MP.png": "b26fba0f741b8b5ca9e806ff15e97f29",
+"assets/assets/images/Mp2.png": "b57bf031943b45fb3e1bc30fbfa3d581",
+"assets/assets/images/Mp3.png": "f933bdbd94b3c26bfe79fce2976b551d",
 "assets/assets/images/My-4-Digit-7-Segment-Control.jpg": "3a71d322f9ed21093a6cbc5a3ee121cd",
 "assets/assets/images/My-LED-Matrix-Designer.jpg": "a71bbc8f70d932f5c48b775b4f515f0b",
 "assets/assets/images/My-Mini-Radar.gif": "75c67c1777dab9f3805bc9fcd320e9b8",
@@ -83,9 +86,9 @@ const RESOURCES = {
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "80cda25cac31ac2115475c93330e570f",
-"/": "80cda25cac31ac2115475c93330e570f",
-"main.dart.js": "2a045fcf8e0132ec6039b64830a8177a",
+"index.html": "beacaf8a117eb355f1364b3552f64400",
+"/": "beacaf8a117eb355f1364b3552f64400",
+"main.dart.js": "ca120371381efa4b718ead24163c7c0e",
 "manifest.json": "7268d731e85213fad6b35f4c3b52f2e6",
 "version.json": "9b818ca9511483c901bed1545384376c"
 };
