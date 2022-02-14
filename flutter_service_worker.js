eefc2543536f1654a781f1b03a6fafd12cf174a7
samir-benabadji <78525925+samir-benabadji@users.noreply.github.com>
2022-02-14 09:10:40 -0800

    Update

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -88,9 +88,9 @@ const RESOURCES = {
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "e8eaa9d1043dd9e8e959773f923f3334",
-"/": "e8eaa9d1043dd9e8e959773f923f3334",
-"main.dart.js": "2e76ec5328ec157dc62e6e12e7ab6616",
+"index.html": "4ac7c6bd651fb6009be3fabeea2e905a",
+"/": "4ac7c6bd651fb6009be3fabeea2e905a",
+"main.dart.js": "e809c62a83665c453b3297ae86cbb131",
 "manifest.json": "7268d731e85213fad6b35f4c3b52f2e6",
 "version.json": "9b818ca9511483c901bed1545384376c"
 };
